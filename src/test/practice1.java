27c56b28a91ff2781def5dfd103c254458554017
being0606 <onuhjin@gmail.com>
2022-11-13 00:04:58 +0900

    This is commit message. [being]

--- src/test/practice1.java
+++ src/test/practice1.java
@@ -5,6 +5,7 @@ public class practice1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		//this is test to Java class
+		System.out.println("this is first time to JAVA to uploading on github.");
 	}
 
 }
